1ff47979919ddcc34cc2d8b6860e8374d97679ba
Nicholas <nicholascode68@gmail.com>
2023-07-06 11:01:42 +0800

    app init

---
+++ Brainstem-substrate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brainstem-substrate
+//
+//  Created by macos on 7/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
