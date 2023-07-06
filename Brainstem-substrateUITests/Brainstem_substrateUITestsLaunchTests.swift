d66dd57d6dca7f6d8653472802f9b999b37cd1e7
Nicholas <nicholascode68@gmail.com>
2023-07-06 11:02:10 +0800

    app test init

---
+++ Brainstem-substrateUITests/Brainstem_substrateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Brainstem_substrateUITestsLaunchTests.swift
+//  Brainstem-substrateUITests
+//
+//  Created by macos on 7/6/23.
+//
+
+import XCTest
+
+class Brainstem_substrateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
