7f3c7135030924a64e407befa763bbf6acdbe017
Nicholas <nicholascode68@gmail.com>
2023-07-06 11:06:27 +0800

    add button actions

--- Brainstem-substrate/MainViewController.swift
+++ Brainstem-substrate/MainViewController.swift
@@ -15,7 +15,11 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func actionCreateWallet(_ sender: Any) {
+    }
+    
+    @IBAction func actionConnectPeaqNetwork(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
